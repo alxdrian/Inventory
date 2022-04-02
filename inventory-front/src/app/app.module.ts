9b7515bab60a56f5a47ba4dff60537fcf87e8711
alxdrian <alex_97piscis@outlook.com>
2022-04-02 17:29:40 -0500

    create create-product component

--- inventory-front/src/app/app.module.ts
+++ inventory-front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProductComponent } from './shared/components/create-product/create-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
