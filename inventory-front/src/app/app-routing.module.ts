bb14ab4312d312599e25953d2cc13eb5c45bb0b4
alxdrian <alex_97piscis@outlook.com>
2022-04-02 18:48:21 -0500

    edit list-products component

--- inventory-front/src/app/app-routing.module.ts
+++ inventory-front/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./core/home/home.module').then(m => m.HomeModule) },
-  { path: 'products', loadChildren: () => import('./core/products/products.module').then(m => m.ProductsModule) }];
+  { path: 'products', loadChildren: () => import('./core/products/products.module').then(m => m.ProductsModule) },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
